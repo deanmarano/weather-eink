7ad18ecae3be25d54663ffb223f3b9aa8552acf0
Dean Marano <dean.marano@gmail.com>
2021-07-21 10:34:57 -0400

    UI tweaks, auto updating.

--- ember/app/components/eink-canvas/component.js
+++ ember/app/components/eink-canvas/component.js
@@ -126,33 +126,34 @@ export default class EinkCanvasComponent extends Component {
     this.context = canvas.getContext('2d');
     this.context.fillStyle = 'white';
     this.context.fillRect(0, 0, 240, 416);
-    this.context.font = '48px serif';
+    this.context.font = '42px serif';
     this.context.fillStyle = 'black';
     this.context.fillText(`it's ${Math.floor(this.args.data.current.temp)}°`, 10, 50);
-    this.context.font = '36px serif';
-    this.context.fillText('but it feels like', 10, 90);
-    this.context.font = '48px serif';
-    this.context.fillText(`${this.args.data.current.feels_like}°`, 60, 140);
+    this.context.font = '24px serif';
+    if(Math.abs(this.args.data.current.temp - this.args.data.current.feels_like) > 5) {
+      this.context.fillText('but it feels like', 10, 90);
+      this.context.font = '42px serif';
+      this.context.fillText(`${this.args.data.current.feels_like}°`, 60, 140);
+    }
     let icon = this.args.data.current.weather.firstObject.icon;
     let url = `https://openweathermap.org/img/wn/${icon}.png`;
     let image = new Image();
     image.crossOrigin = 'anonymous'; // This enables CORS
     image.onload = () => {
-      this.context.drawImage(image, 10, 100);
+      this.context.drawImage(image, 240 / 2 - 100, 200, 200, 200);
+      this.dataurl = 'data:image/bmp;base64,' + btoa(createBMP(this.canvas));
     };
     image.src = url;
-    this.dataurl = 'data:image/bmp;base64,' + btoa(createBMP(this.canvas));
   }
 
   @action
   download(event) {
-    let currentDate = new Date();
-    let cDay = currentDate.getDate();
-    let cMonth = currentDate.getMonth() + 1;
-    let cYear = currentDate.getFullYear();
-    event.target.download = `${cDay}/${cMonth}/${cYear}`;
-    let bmp = this.canvas.toDataURL('image/bmp');
-
+    // let currentDate = new Date();
+    // let cDay = currentDate.getDate();
+    // let cMonth = currentDate.getMonth() + 1;
+    // let cYear = currentDate.getFullYear();
+    //event.target.download = `${cYear}.${cMonth}.${cDay}.bmp`;
+    event.target.download = `dark.bmp`;
     event.target.href = this.dataurl;
   }
 }
